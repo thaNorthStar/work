c06e038fdad1a8c6874fe9e114a109b23bde49c2
thaNorthStar <pbueltel@paulbueltel.com>
2022-11-11 09:59:17 -0600

    created classes for players guesses & ref, need to add methods to ref class to generate random number & determine winner

--- project-1/src/github/GuessGame.java
+++ project-1/src/github/GuessGame.java
@@ -3,69 +3,65 @@ package github;
 import java.util.*;
 
 
-//
-//class Player 
-//{
-//	int numOfPlayers;
-//	int guesses;
-//	
-//	
-//	int getGameSetup()
-//	{
-//		Scanner scan1 = new Scanner(System.in);
-//		System.out.println("How many players?");
-//		int numOfPlayers = scan1.nextInt();
-//		int [][]playerArray= new int[numOfPlayers][1];
-//
-//		for(int i=0;i<playerArray.length;i++)
-//		{
-//			for(int j=0; j<playerArray[i].length;j++)
-//			{
-//				Scanner scan2 = new Scanner(System.in);
-//				System.out.println("Player "+(1+i)+", enter your guess: ");
-//				playerArray[i][j] = scan2.nextInt();
-//			}
-//		}
-//		System.out.println("Player guesses: "+numOfPlayers);
-//	}
-//}
-
-//
-//
-class Ref
+class Players
 {
 	
-	class Input
+	int numOfPlayers;
+	
+	int getNumOfPlayers()
 	{
-		public void numOfPlayers()
-		{
-			Scanner scan1 = new Scanner(System.in);
-			System.out.println("How many players?");
-			int numOfPlayers = scan1.nextInt();
-			int []guesses= new int[numOfPlayers];
-			
-			for(int i=0;i<guesses.length;i++)
-			{
-				Scanner scan2 = new Scanner(System.in);
-				System.out.println("Player "+(1+i)+", what is your guess?");
-				guesses[i]= scan2.nextInt();
-			}
-			System.out.println("The "+numOfPlayers+" players' guesses are:" );
-			for(int elem:guesses)
-			{
-				System.out.println(elem);
-			}
-		}
+		Scanner scan1 = new Scanner(System.in);
+		System.out.println("How many players?");
+		numOfPlayers = scan1.nextInt();
+		
+		return numOfPlayers;
 	}
-	//
 }
+class Guesses
+{
+	int []guesses;
+	
+	int[] getGuesses(int numOfPlayers)
+	{
+		int []guesses= new int[numOfPlayers];
+		for(int i=0;i<guesses.length;i++)
+		{
+			Scanner scan2 = new Scanner(System.in);
+			System.out.println("Player "+(1+i)+", what is your guess?");
+			guesses[i]= scan2.nextInt();
+		}
+		return guesses;
+	}
+}
+
+class Ref
+{
+	int answer;
+	int winner;
+}
+
+//	
+//	for(int elem:guesses)
+//	{
+//		System.out.println(elem);
+//	}
+//	return guesses;
+//
+//
+//class Ref
+//{
+//	int answer;
+//	
+//
+//	//
+//}
 public class GuessGame 
 {
 	public static void main(String[] args)
 	{
-		Ref obj = new Ref();
+//		Ref obj = new Ref();
 		
-		Ref.Input obj1 = obj.new Input();
+		Ref.Input obj1 = new Ref.Input();
 		obj1.numOfPlayers();
 		
 
